f74b7abec74eeaf7c209a6166a7f4666acb181aa
Absoluten00b <whiteviolent8@gmail.com>
2024-02-17 09:47:42 +0530

    init

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#               2022-2024 The PixelOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_lemonade.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_lemonade-user \
+    aosp_lemonade-userdebug \
+    aosp_lemonade-eng
